04482ff1de9b3ef9b4c27822d80461d5742221bf
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-09-14 23:33:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Docker World!\n');
+  res.end('Welcome to the Dockerized Node.js App!\n');
 });
 
 const port = process.env.PORT || 3000;
